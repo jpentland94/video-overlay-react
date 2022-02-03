239d24b7a1343e935c974d896aa2c66159fff204
jpentland94 <98651757+jpentland94@users.noreply.github.com>
2022-02-03 00:18:49 +0000

    basic folder restructure

---
+++ src/components/VideoBlock.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../styles/Video.module.css";
+
+function VideoBlock() {
+  return <video className={styles.video} src=""></video>;
+}
+
+export default VideoBlock;
